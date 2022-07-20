3589045247798ea60dd5d146b8a80f5935046f0a
Ujjwal Karki <ujjwalkarki.56@gmail.com>
2022-07-20 11:06:51 +0545

    js challenger object solution

---
+++ Object/7seven.js
@@ -0,0 +1,14 @@
+// Write a function that takes two strings (a and b) as arguments
+// Create an object that has a property with key 'a' and a value of 'b'
+// Return the object
+
+
+function myFunction(a,b){
+     
+    return {[a]:b};
+}
+
+
+console.log(myFunction('a','b'))   //output:{a:'b'}
+console.log(myFunction('z','x'))   //output:{z:'x'}
+console.log(myFunction('b','w'))   //output:{b:'w'}
